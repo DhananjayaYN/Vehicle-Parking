49a8a1af9f6001e30c85b54ffe02b8f66dd76050
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2024-07-04 17:35:16 +0530

    Update NaviBar.css and NaviBar.js

--- src/Components/NaviBar.js
+++ src/Components/NaviBar.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import './NaviBarCSS.css';
+import React from "react";
+import "./NaviBar.css"
+const NaviBar = () => {
+    return (
+        <div className="box">
+            <div className="tabBox">
+                <div className="tab">Hi Nimnatha</div>
+            </div>
+            <div className="tabBox">
+                <div className="tab">Parking Place</div>
+            </div>
+            <div className="tabBox">
+                <div className="tab">About Us</div>
+            </div>
+            <div className="tabBox">
+                <div className="tab">My Plans</div>
+            </div>
+            <div className="tabBox">
+                <div className="tab">Home</div>
+            </div>
 
-export default function NaviBar() {
-  return (
-    <div class='NaviBar'>
-        <ul class='NaviList'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>My Booking</a></li>
-            <li><a href='#'>About Us</a></li>
-            <li><a href='#'>Login</a></li>
-            <div class='right_navi_item'><li><a href='#'>Signup</a></li></div>
-        </ul>
-        <hr/>
-    </div>
-  )
+        </div>
+    );
 }
+export default NaviBar;
