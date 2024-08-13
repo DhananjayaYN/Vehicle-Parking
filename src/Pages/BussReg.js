0fe81701b95e06b7daa5ea45f33dc1d6c1d03451
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2024-08-13 20:10:03 +0530

    Update BussReg.js

--- src/Pages/BussReg.js
+++ src/Pages/BussReg.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import "./BussReg.css"
 import Footer from '../Components/Footer';
 import logo from '../Images/inner logo.svg'
 import watermark from '../Images/hidden logo.svg'
-import NaviBar from '../Components/NaviBar';
+import NewNaviBar from '../Components/NewNaviBar';
 import V from '../Images/V.svg'
+import emailIcon from '../Images/email_Icon.svg'
+import Phone_Icon from '../Images/Phone_Icon.svg'
+
 const BussReg = () => {
     const [searchValue, setSearchValue] = useState();
+
+    //image
+    const [image, setImage] = useState();
+    const inputRef = useRef(null);
+    const changeimage = (e) => {
+        setImage(e.target.files[0]);
+    }
+
+
     return (
         <div className='Home'>
             <div className='main_box'>
@@ -26,7 +38,7 @@ const BussReg = () => {
                 </div>
                 <div className='rightside_box'>
                     <div className='navibar_box'>
-                        <NaviBar />
+                        <NewNaviBar />
                     </div>
                     <div className='container'>
                         <div className='containerBody'>
@@ -34,17 +46,25 @@ const BussReg = () => {
                                 <div className='card11'>
                                     <div className='frame11'>
                                         <div className='frame11_body'>
-                                            Enter company Name : <input type='text' /><br />
-                                            Enter your name : <input type='text' /><br />
-                                            Enter email : <input type='text' /><br />
-                                            Enter phone number : <input type='text' /><button>Verify</button><br />
-                                            <button>Update Details</button>
+                                            <div className='frame11_body_contnts'>
+                                                <div className='frame11_body_01'>
+                                                    <p>Enter company Name :<span className='input1'><input type='text' /></span></p>
+                                                    <p>Enter your name :<span className='input2'><input type='text' /></span></p>
+                                                    <p> Enter email :<span className='iconimage1' ><img src={emailIcon} style={{ width: 20, height: 20, position: 'relative', top: 5 }} /></span><span className='input3'><input type='text' /></span></p>
+                                                    <p>Enter phone number :<span className='iconimage2'><img src={Phone_Icon} style={{ width: 18, height: 18, position: 'relative', top: 5 }} /></span><span className='input4'><input type='text' /></span><span className='btn'><button>Verify</button></span> </p>
+                                                    <button className='button11'>Update Details</button>
+
+                                                </div>
+                                            </div>
+
                                         </div>
+
                                     </div>
                                 </div>
                                 <div className='card12'>
                                     <div className='frame12'>
-                                        <img src={V} />
+                                        {image ? <img src={URL.createObjectURL(image)} onClick={() => { inputRef.current.click(); }} className='image1' /> : <img src={V} onClick={() => { inputRef.current.click(); }} className='image1' />}
+                                        <input type='file' ref={inputRef} onChange={changeimage} style={{ display: "none" }} />
                                     </div>
                                 </div>
                             </div>
