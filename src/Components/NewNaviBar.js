808db9b09000ceb8495eacca3e4e2b43ba470499
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2024-08-13 20:05:08 +0530

    Create NewNaviBar.js and NewNaviBar.css

---
+++ src/Components/NewNaviBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./NewNaviBar.css"
+const NewNaviBar = () => {
+    return (
+        <div className="box">
+            <div className="tabBox">
+                <div className="tab">Hi Nimnatha</div>
+            </div>
+            <div className="tabBox">
+                <div className="tab">Parking Place</div>
+            </div>
+            <div className="tabBox">
+                <div className="tab">About Us</div>
+            </div>
+            <div className="tabBox">
+                <div className="tab">My Plans</div>
+            </div>
+            <div className="tabBox">
+                <div className="tab">Home</div>
+            </div>
+
+        </div>
+    );
+}
+export default NewNaviBar;
