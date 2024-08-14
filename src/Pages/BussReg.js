09cf47e600f5562a3c3c7b04e1875a7ba0d0bce7
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2024-08-14 08:47:35 +0530

    Update BussReg.js

--- src/Pages/BussReg.js
+++ src/Pages/BussReg.js
@@ -1,21 +1,60 @@
-import React, { useState, useRef } from 'react'
-import "./BussReg.css"
+import React, { useState, useRef } from 'react';
+import "./BussReg.css";
 import Footer from '../Components/Footer';
-import logo from '../Images/inner logo.svg'
-import watermark from '../Images/hidden logo.svg'
+import logo from '../Images/inner logo.svg';
+import watermark from '../Images/hidden logo.svg';
 import NewNaviBar from '../Components/NewNaviBar';
-import V from '../Images/V.svg'
-import emailIcon from '../Images/email_Icon.svg'
-import Phone_Icon from '../Images/Phone_Icon.svg'
+import V from '../Images/V.svg';
+import emailIcon from '../Images/email_Icon.svg';
+import Phone_Icon from '../Images/Phone_Icon.svg';
+import Car_Icon from '../Images/Car_Icon.svg';
+import Bike_Icon from '../Images/Bike_Icon.svg';
+import Three_wheel_Icon from '../Images/Three_wheel_Icon.svg';
+import bin_Icon from '../Images/bin_Icon.svg'
 
 const BussReg = () => {
     const [searchValue, setSearchValue] = useState();
+
+    //Icons
+    const [selectvalue, setselectvalue] = useState();
+    const [showCar, setshowCar] = useState(true);
+    const chngecarvalue = () => {
+        setshowCar(true);
+        setselectvalue("");
+    }
+    const chngecarvalue2 = () => {
+        setshowCar(false);
+    }
+
+    const [showBike, setshowBike] = useState(true);
+    const chngebikevalue = () => {
+        setshowBike(true);
+        setselectvalue("");
+    }
+    const chngebikevalue2 = () => {
+        setshowBike(false);
+    }
+    const [showThreeWheel, setshowThreeWheel] = useState(true);
+    const chngethreewheelvalue = () => {
+        setshowThreeWheel(true);
+        setselectvalue("");
+    }
+    const chngethreewheelvalue2 = () => {
+        setshowThreeWheel(false);
+    }
+
+    //bank
+    const [bank, setbank] = useState();
+
+    //branch
+    const [branch, setbranch] = useState();
 
     //image
     const [image, setImage] = useState();
     const inputRef = useRef(null);
     const changeimage = (e) => {
         setImage(e.target.files[0]);
+
     }
 
 
@@ -73,15 +112,25 @@ const BussReg = () => {
                                     <br />
                                     <div className='frame21'>
                                         <div className='frame21_body'>
-                                            Enter Address : <input type='text' /><br />
-                                            Add Vehicle Types <br />
-                                            Charging Fee (Slot per hour) :<input type='text' /><input type='text' /><br />
-                                            Company Bank Details : <br />
-                                            <button>Update Details</button>
+                                            <div className='frame21_body_01'>
+                                                <p>Enter Address : <span className='input5'><input type='text' value={branch} /></span></p>
+                                                <p>Add Vehicle Types <span className='vehicle_type'><span><select onChange={(e) => { setselectvalue(e.target.value) }}><option></option><option value="Car"  >Car </option><option value="Bike">Bike</option><option value="Three wheel">Three wheel</option></select></span> {(selectvalue == "Car") && <span className='iconimage3' ><img src={Car_Icon} style={{ width: 20, height: 20, position: 'relative', top: 5 }} /> <input type='checkbox' onClick={chngecarvalue} /></span>} {(selectvalue == "Bike") && <span className='iconimage4' ><img src={Bike_Icon} style={{ width: 20, height: 20, position: 'relative', top: 5 }} /><input type='checkbox' onClick={chngebikevalue} /></span>}{(selectvalue == "Three wheel") && <span className='iconimage5' ><img src={Three_wheel_Icon} style={{ width: 20, height: 20, position: 'relative', top: 5 }} /><input type='checkbox' onClick={chngethreewheelvalue} /></span>}<span className='showCar'>{(showCar) && <span className='iconimage3' ><img src={Car_Icon} style={{ width: 20, height: 20, position: 'relative', top: 5 }} /><span className='iconimage3' ><img src={bin_Icon} onClick={chngecarvalue2} style={{ width: 16, height: 16, position: 'relative', top: 5 }} /></span></span>}</span><span className='showBike'>{(showBike) && <span className='iconimage3' ><img src={Bike_Icon} style={{ width: 20, height: 20, position: 'relative', top: 5 }} /><span className='iconimage3' ><img src={bin_Icon} onClick={chngebikevalue2} style={{ width: 16, height: 16, position: 'relative', top: 5 }} /></span></span>}</span><span className='showThreeWheel'>{(showThreeWheel) && <span className='iconimage3' ><img src={Three_wheel_Icon} style={{ width: 20, height: 20, position: 'relative', top: 5 }} /><span className='iconimage3' ><img src={bin_Icon} onClick={chngethreewheelvalue2} style={{ width: 16, height: 16, position: 'relative', top: 5 }} /></span></span>}</span></span></p>
+                                                <p>Charging Fee (Slot per hour) :<span className='Slotperhour'>{(showCar) && <span className='input6'><input type='text' /></span>}{(showBike) && <span className='input7'><input type='text' /></span>}{(showThreeWheel) && <span className='input8'><input type='text' /></span>}</span></p>
+                                                <p>Company Bank Details :</p>
+                                                <div>
+                                                    <ul style={{ listStyleType: 'none' }}>
+                                                        <li><p>Bank  <span className='bank'><select onChange={(e) => { setbank(e.target.value) }}><option></option><option value="Bank of Ceylon">Bank of Ceylon</option><option value="Commercial Bank">Commercial Bank</option><option value="Hatton National Bank">Hatton National Bank</option><option value="People's Bank">People's Bank</option><option value="Sampath Bank">Sampath Bank</option><option value="National Development Bank">National Development Bank</option><option value="Union Bank">Union Bank</option><option value="DFCC Bank">DFCC Bank</option><option value="Cargills Bank">Cargills Bank</option></select></span></p></li>
+                                                        <li><p>Branch <span className='branch'><select onChange={(e) => { setbranch(e.target.value) }}><option></option><option value="Bank of Ceylon - Colombo Fort">Bank of Ceylon - Colombo Fort</option><option value="Bank of Ceylon - Kandy">Bank of Ceylon - Kandy</option><option value="Bank of Ceylon - Galle">Bank of Ceylon - Galle</option><option value="Commercial Bank - Colombo Main">Commercial Bank - Colombo Main</option><option value="Commercial Bank - Nugegoda">Commercial Bank - Nugegoda</option><option value="Commercial Bank - Jaffna">Commercial Bank - Jaffna</option><option value="Hatton National Bank - Head Office">Hatton National Bank - Head Office</option><option value="Hatton National Bank - Mount Lavinia">Hatton National Bank - Mount Lavinia</option><option value="Hatton National Bank - Kandy">Hatton National Bank - Kandy</option><option value="People's Bank - Colombo Fort">People's Bank - Colombo Fort</option><option value="People's Bank - Rajagiriya">People's Bank - Rajagiriya</option><option value="People's Bank - Matara">People's Bank - Matara</option><option value="Sampath Bank - Colombo 3">Sampath Bank - Colombo 3</option><option value="Sampath Bank - Battaramulla">Sampath Bank - Battaramulla</option><option value="Sampath Bank - Anuradhapura">Sampath Bank - Anuradhapura</option><option value="National Development Bank - Colombo">National Development Bank - Colombo</option><option value="National Development Bank - Negombo">National Development Bank - Negombo</option><option value="National Development Bank - Gampaha">National Development Bank - Gampaha</option><option value="Union Bank - Colombo">Union Bank - Colombo</option><option value="Union Bank - Kandy">Union Bank - Kandy</option><option value="Union Bank - Jaffna">Union Bank - Jaffna</option><option value="DFCC Bank - Colombo">DFCC Bank - Colombo</option><option value="DFCC Bank - Galle">DFCC Bank - Galle</option><option value="DFCC Bank - Kandy">DFCC Bank - Kandy</option><option value="Cargills Bank - Colombo">Cargills Bank - Colombo</option><option value="Cargills Bank - Kandy">Cargills Bank - Kandy</option><option value="Cargills Bank - Jaffna">Cargills Bank - Jaffna</option>
+                                                        </select></span></p></li>
+                                                        <li><p>Name<span className='input9'><input type='text' /></span></p></li>
+                                                        <li><p>Account Number<span className='input10'><input type='text' /></span></p></li>
+                                                    </ul>
+                                                </div>
+                                                <p><span className='Updatebtn'><button>Update Details</button></span></p>
+                                            </div>
                                         </div>
                                     </div>
-                                    <br />
-                                    <button>Create My Parking Space</button>
+                                    <p><span className='CreateMyParkingSpacebtn'><button>Create My Parking Space</button></span></p>
                                     <br />
                                 </div>
                                 <div className='card22'>
@@ -89,12 +138,12 @@ const BussReg = () => {
                                         <div className='frame22_body' style={{ alignSelf: 'center', }}>
                                             One Galle Face<br /><br />
                                             Parking  History <br /><br />
-                                            Date:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;In:<br />
-                                            <input type='text' /><br /><input type='text' /><br />
+                                            <span>Date:</span><span className='Intext'>In:</span><br />
+                                            <p><span className='input11'><input type='text' /></span><span className='input12'><input type='text' /></span></p>
                                             <button>View</button><br />
                                             Earnings <br />
-                                            Date:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;In:<br />
-                                            <input type='text' /><br /><input type='text' /><br />
+                                            <span>Date:</span><span className='Intext'>In:</span><br />
+                                            <p><span className='input13'><input type='text' /></span><span className='input14'><input type='text' /></span></p>
                                             <button>View</button><br />
                                         </div>
                                     </div>
@@ -106,7 +155,7 @@ const BussReg = () => {
                     </div>
                 </div>
             </div>
-            <Footer />
+            <Footer className="footer" />
         </div>
     )
 }
