de5261e4dc109e3040ff860ca94572ae73402112
Nimesh <110623261+DhananjayaYN@users.noreply.github.com>
2024-08-15 10:02:07 +0530

    add comment to the app,js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@
 //import NaviBar from "./Components/NaviBar"
 //import Footer from "./Components/Footer";
 //import SideBarUser from "./Components/SideBarUser";
+//---------Nimesh--------
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import HomeCustomer from "./Pages/HomeCustomer";
-import Places_card from "./Components/Places_card";
+//---------Nimantha--------
 import BussReg from "./Pages/BussReg";
+//---------Sahan------------
 import UserLogin from "./Pages/UserLogin";
 import OwnerLogin from "./Pages/OwnerLogin";
 import UserSignup from "./Pages/UserSignup";
 import OwnerSignup from "./Pages/OwnerSignup";
+//---------Hansa------------
 import CustomerPage from './Pages/CustomerPage'
 
 function App() {
@@ -20,7 +23,6 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage/>}></Route>
           <Route path="/customer" element={<HomeCustomer/>}></Route>
-          <Route path="/card" element={<Places_card/>}></Route>
           <Route path="/owner" element={<BussReg/>}></Route>
           <Route path="/userLogin" element={<UserLogin/>}></Route>
           <Route path="/ownerLogin" element={<OwnerLogin/>}></Route>
