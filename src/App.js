d2606ff7972cfc6a396636190ac165c4b854fac7
Nimesh <110623261+DhananjayaYN@users.noreply.github.com>
2024-08-15 09:59:09 +0530

    routing the signup,login pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import HomePage from "./Pages/HomePage";
 import HomeCustomer from "./Pages/HomeCustomer";
 import Places_card from "./Components/Places_card";
 import BussReg from "./Pages/BussReg";
-import LoginPage from "./Pages/Login";
+import UserLogin from "./Pages/UserLogin";
+import OwnerLogin from "./Pages/OwnerLogin";
+import UserSignup from "./Pages/UserSignup";
+import OwnerSignup from "./Pages/OwnerSignup";
 import CustomerPage from './Pages/CustomerPage'
 
 function App() {
@@ -19,7 +22,10 @@ function App() {
           <Route path="/customer" element={<HomeCustomer/>}></Route>
           <Route path="/card" element={<Places_card/>}></Route>
           <Route path="/owner" element={<BussReg/>}></Route>
-          <Route path="/login" element={<LoginPage/>}></Route>
+          <Route path="/userLogin" element={<UserLogin/>}></Route>
+          <Route path="/ownerLogin" element={<OwnerLogin/>}></Route>
+          <Route path="/userSignup" element={<UserSignup/>}></Route>
+          <Route path="/ownerSignup" element={<OwnerSignup/>}></Route>
           <Route path="/customer/select-vehicle" element={<CustomerPage/>}></Route>
         </Routes>
       </BrowserRouter>
