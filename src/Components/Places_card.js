5b52e51fc7dd9033a74a002ec552da725697c7cb
Nimesh <110623261+DhananjayaYN@users.noreply.github.com>
2024-08-22 12:19:24 +0530

    add fetchOwnerShop fuction

--- src/Components/Places_card.js
+++ src/Components/Places_card.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -6,17 +6,40 @@ import './Place_cardCSS.css';
 
 
 // Sample array of card data
-const cardData = [
-    { id: 1, title: 'Card 1', content: 'This is the first card' },
-    { id: 2, title: 'Card 2', content: 'This is the second card' },
-    { id: 3, title: 'Card 3', content: 'This is the third card' },
-    { id: 4, title: 'Card 4', content: 'This is the fourth card' },
-    { id: 5, title: 'Card 5', content: 'This is the fifth card' },
-    { id: 6, title: 'Card 6', content: 'This is the sixth card' },
-    { id: 7, title: 'Card 7', content: 'This is the seventh card' },
-];
+// const cardData = [
+//     { id: 1, title: 'Card 1', content: 'This is the first card' },
+//     { id: 2, title: 'Card 2', content: 'This is the second card' },
+//     { id: 3, title: 'Card 3', content: 'This is the third card' },
+//     { id: 4, title: 'Card 4', content: 'This is the fourth card' },
+//     { id: 5, title: 'Card 5', content: 'This is the fifth card' },
+//     { id: 6, title: 'Card 6', content: 'This is the sixth card' },
+//     { id: 7, title: 'Card 7', content: 'This is the seventh card' },
+// ];
 
 export default function Places_card() {
+
+    // useState hook to manage the card data
+    const [cardData, setCardData] = useState([]);
+
+    // useEffect hook to fetch data from the backend when the component mounts
+    useEffect(() => {
+        // Make a GET request to the backend API to fetch data from the 'owner' collection
+        const fetchOwnerShop = async () => {
+          const response = await fetch(`/api/owners/`);
+          const jsona = await response.json();
+    
+          if(response.ok){
+            // Update the state with the fetched data
+            setCardData(jsona);
+          }else{
+            console.log(jsona);
+          }
+          console.log(cardData);
+        }
+        fetchOwnerShop();
+
+    }, []); // Empty dependency array means this effect runs once after the initial render
+
      // Settings for the react-slick slider
     const settings = {
         dots: false,
